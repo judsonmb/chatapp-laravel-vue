9db93b54e3aedd3ac08eacf07c98d430eb16376e
Judson <judsonmelobandeira@gmail.com>
2022-04-24 11:10:07 -0300

    first commit

---
+++ resources/js/Pages/Chat/messageContainer.vue
@@ -0,0 +1,20 @@
+<script setup>
+import messageItem from './messageItem.vue';
+</script>
+<template>
+    <div class="h-96 w-full">
+        <div class="h-full p-2 flex flex-col-reverse overflow-scroll">
+            <div v-for="(message, index) in messages" :key="index">
+                <message-item :message="message" />
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    components: { messageItem },
+    props: ['messages']
+}
+</script>
